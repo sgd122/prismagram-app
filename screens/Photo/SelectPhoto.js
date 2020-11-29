cb047ffc2e016df7829b58a7a3cd2ab70a7b14d0
성기동 <sgd0947@gmail.com>
2020-11-29 13:50:25 +0900

    #18.3 Take Photo part One

--- screens/Photo/SelectPhoto.js
+++ screens/Photo/SelectPhoto.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Image, ScrollView } from "react-native";
+import { Image, ScrollView, TouchableOpacity } from "react-native";
 import * as Permissions from 'expo-permissions';
 import * as MediaLibrary from 'expo-media-library';
 import styled from "styled-components";
@@ -10,13 +10,14 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
-
 export default () => {
   const [loading, setLoading] = useState(true);
   const [hasPermission, setHasPermission] = useState(false);
   const [selected, setSelected] = useState();
   const [allPhotos, setAllPhotos] = useState();
+  const changeSelected = (photo) => {
+    setSelected(photo);
+  }
   const getPhotos = async () => {
     try {
       const { assets } = await MediaLibrary.getAssetsAsync();
@@ -62,14 +63,16 @@ export default () => {
 
               <ScrollView contentContainerStyle={{ flexDirection: "row" }}>
                 {allPhotos.map(photo =>
-                  <Image
-                    key={photo.id}
-                    source={{ uri: photo.uri }}
-                    style={{
-                      width: constants.width / 3,
-                      height: constants.height / 6
-                    }}
-                  />
+                  <TouchableOpacity key={photo.id} onPress={() => changeSelected(photo)}>
+                    <Image
+                      source={{ uri: photo.uri }}
+                      style={{
+                        width: constants.width / 3,
+                        height: constants.height / 6,
+                        opacity: photo.id === selected.id ? 0.5 : 1
+                      }}
+                    />
+                  </TouchableOpacity>
                 )}
               </ScrollView>
             </>
--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,19 +1,39 @@
-import * as React from "react";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React, { useState, useEffect } from "react";
+import { Image, ScrollView, TouchableOpacity } from "react-native";
+import * as Permissions from 'expo-permissions';
 import styled from "styled-components";
+import { Camera } from 'expo-camera';
+import Loader from "../../components/Loader";
+import constants from "../../constants";
 
 const View = styled.View`
-  justify-content: center;
-  align-items: center;
   flex: 1;
 `;
 
 const Text = styled.Text``;
 
-export default ({ navigation }) => (
-  <View>
-    <TouchableOpacity onPress={() => navigation.navigate("UploadPhoto")}>
-      <Text>TakePhoto</Text>
-    </TouchableOpacity>
-  </View>
-);
+export default () => {
+  const [loading, setLoading] = useState(true);
+  const [hasPermission, setHasPermission] = useState(false);
+  const askPermission = async () => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      if (status === "granted") {
+        setHasPermission(true);
+      }
+    } catch (e) {
+      console.log(e);
+      hasPermission(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    askPermission();
+  }, []);
+  return (
+    <View>
+      {loading ? <Loader /> : hasPermission ? <Camera styles={{ width: constants.width, height: constants.height / 2 }} /> : null}
+    </View>
+  );
+};
